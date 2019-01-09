a628c8e88a8683f8e2903ade38a3bb25f842ea68
Paul Bright <paul@sons.co.nz>
2019-01-10 12:40:47 +1300

    get_checkout_inlines

--- dps/__init__.py
+++ dps/__init__.py
@@ -1,3 +1,8 @@
 from ._version import __version__
 
-__all__ = ['__version__']
+__all__ = ['__version__', 'get_checkout_inlines']
+
+
+def get_checkout_inlines():
+    from .admin import TransactionInlineAdmin
+    return [TransactionInlineAdmin]
